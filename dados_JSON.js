12bc49f313a8a2c7e74e9f781539dee7d05c44f7
Lucas_Carvalho <161785293+carvalhosouzalucas@users.noreply.github.com>
2025-10-22 16:47:57 -0300

    Add files via upload

---
+++ dados_JSON.js
@@ -0,0 +1,12 @@
+const pessoa = {
+    "nome": "João",
+    "sobrenome": "Silva Medeiros",
+    "sexo": "Masculino",
+    "CPF": "542.367.001-71",
+    "RG": "12.508.060-9",
+    "telefone": "015 (11) 9902-5674",
+    "email": "joaosilva@email.com"
+
+};
+
+console.log(pessoa);
